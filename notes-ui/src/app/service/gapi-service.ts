281c32d5506dcb266b81242cd5ec48367f664933
Ajeet Singh <ajeet.cloud@gmail.com>
2023-06-11 20:39:30 +0530

    Added code for pagination on scroll up

--- notes-ui/src/app/service/gapi-service.ts
+++ notes-ui/src/app/service/gapi-service.ts
@@ -1,32 +1,9 @@
-import {Injectable, NgZone} from "@angular/core";
+import {Injectable} from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GapiService {
 
-
-  constructor(private ngZone: NgZone) {
-  }
-
-
-  public signIn() {
-
-    /*   gapi.client.init({
-         'apiKey': 'AIzaSyB0PSoTskV9uj1LbUirp86hrWPVO3FW0UQ',
-         'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-         'clientId': '1016033627023-u8eibbvg9l1cmurskqrmfql5ndtquaap.apps.googleusercontent.com',
-         'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-       }).then(function (response: any) {
-         console.log(response);
-       }, function (reason: any) {
-         console.log('Error: ' + reason.result.error.message);
-       });*/
-  }
-
-
-  private signInSuccessHandler() {
-
-  }
 
 }
