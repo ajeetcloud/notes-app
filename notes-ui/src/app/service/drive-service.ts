5e700d4d2751dce0725939ff58d169e09257c329
Ajeet Singh <ajeet.cloud@gmail.com>
2023-07-10 21:13:42 +0530

    Google drive service initial code

--- notes-ui/src/app/service/drive-service.ts
+++ notes-ui/src/app/service/drive-service.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@angular/core";
 import {Subject} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -15,5 +14,18 @@ export class DriveService {
   private accessTokenRetrievedSubject = new Subject<void>();
 
   constructor(private http: HttpClient) {
+  }
+
+  authorize() {
+    // @ts-ignore
+    const client = google.accounts.oauth2.initCodeClient({
+      client_id: '',
+      scope: '',
+      ux_mode: 'popup',
+      callback: (response: any) => {
+        // TODO: handle callback
+      },
+    });
+    client.requestCode();
   }
 }
