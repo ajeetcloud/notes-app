bfb73e6de454d60dcd8864920ed9921b6896d6e6
Ajeet Singh <ajeet.cloud@gmail.com>
2023-10-20 19:49:32 +0530

    Added Register logic

--- notes-ui/src/app/register/register.component.ts
+++ notes-ui/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from "@angular/core";
 import {SignupRequest} from "../types/types";
 import {Subject, takeUntil} from "rxjs";
 import {LoginService} from "../service/login.service";
+import {SIGNUP_SUCCESSFUL_MSG} from "../common/constants";
 
 
 @Component({
@@ -18,12 +19,16 @@ export class RegisterComponent implements OnInit, OnDestroy {
   email: string = '';
   signupRequest: SignupRequest;
 
+  isRegistered: boolean = false;
+  registrationSuccessfulMsg = '';
+
   private destroyed = new Subject<void>();
 
   constructor(private loginService: LoginService) {
   }
 
   ngOnInit(): void {
+    this.isRegistered = false;
   }
 
   register() {
@@ -37,9 +42,19 @@ export class RegisterComponent implements OnInit, OnDestroy {
     this.loginService.signup(this.signupRequest)
       .pipe(takeUntil(this.destroyed))
       .subscribe((res: SignupRequest) => {
-          // send msg on UI and clean up the form
+          this.cleanup();
+          this.isRegistered = true;
+          this.registrationSuccessfulMsg = SIGNUP_SUCCESSFUL_MSG(res.firstName);
         }
       );
+  }
+
+  cleanup() {
+    this.username = '';
+    this.password = '';
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
   }
 
   ngOnDestroy(): void {
